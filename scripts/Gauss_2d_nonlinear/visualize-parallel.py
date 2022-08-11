0f47a43692f57373b058a0672722f3d93b59649a
ianjazz246 <ianjazz246@gmail.com>
2022-08-11 14:52:23 -0700

    Fix output image renaming

--- scripts/Gauss_2d_nonlinear/visualize-parallel.py
+++ scripts/Gauss_2d_nonlinear/visualize-parallel.py
@@ -4,33 +4,32 @@ import subprocess
 import os
 import re
 
-OUTPUT_H5_FILENAME = "/Gauss2d-ez-000003696.h5"
+OUTPUT_H5_FILENAME = "Gauss2d-ez-000011088.h5"
 
 """
 Replace last occurance of old in string s with new
 """
 def rreplace(s: str, old: str, new: str, occurrence: int) -> str:
-    li = s.rsplit(old, occurrence)
+    li = s.rsplit(old, maxsplit=occurrence)
     return new.join(li)
 
 """
 Runs ../make-visualization.sh script to turn h5 to png.
+mainDataFile must be an absolute path
 Also renames output png to the name of the directory it's in
 """
 def visualize_gauss(mainDataFile: str):
     subprocess.run(["../make-visualization.sh", mainDataFile])
-    # chdir so we don't need to use absolute path when renaming output image
-    os.chdir(os.path.dirname(os.path.abspath(mainDataFile)))
-    containing_dir_name = os.path.basename(os.path.dirname(os.path.abspath(mainDataFile)))
-    os.rename(rreplace(os.path.abspath(mainDataFile), ".h5", ".png", 1),
-              containing_dir_name + ".png")
+    output_png_path = rreplace(mainDataFile, ".h5", ".png", 1)
+    containing_dir_name = os.path.basename(os.path.dirname(mainDataFile))
+    os.rename(output_png_path, os.path.dirname(output_png_path) + "/" + containing_dir_name + ".png")
 
 """
 Get iterator containg directory names that match pattern "amp=* logk=*"
 Only directories in current directory, names relative to current directory
 """
 def get_dirs_to_visualize() -> filter:
-    regex_pattern = "^amp=[0-9\.]+ logk=[+-]?[0-9\.]+$"
+    regex_pattern = "^amp=[0-9\.]+ logk=[+-]?[0-9\.]+ detailed$"
     regex = re.compile(regex_pattern)
     dirs = os.listdir(".")
     matching_dirs = filter(lambda dir: regex.search(dir), dirs)
@@ -38,6 +37,6 @@ def get_dirs_to_visualize() -> filter:
 
 if __name__ == "__main__":
     print("Note. Run this file in directory containing Gauss2d.py and output directories")
-    dirs_to_visualize = map(lambda dir: dir + OUTPUT_H5_FILENAME, get_dirs_to_visualize())
+    files_to_visualize = map(lambda dir: os.path.abspath(dir + "/" + OUTPUT_H5_FILENAME), get_dirs_to_visualize())
     with multiprocessing.Pool() as pool:
-        pool.map(visualize_gauss, dirs_to_visualize)
+        pool.map(visualize_gauss, files_to_visualize)
