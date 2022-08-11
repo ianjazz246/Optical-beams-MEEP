325d96b0eeaaeb5de4d1dbde964bc51ddf7268f8
ianjazz246 <ianjazz246@gmail.com>
2022-08-11 11:59:36 -0700

    Rename image to dir in Visualize-parallel

--- scripts/Gauss_2d_nonlinear/visualize-parallel.py
+++ scripts/Gauss_2d_nonlinear/visualize-parallel.py
@@ -4,6 +4,8 @@ import subprocess
 import os
 import re
 
+OUTPUT_H5_FILENAME = "/Gauss2d-ez-000003696.h5"
+
 """
 Replace last occurance of old in string s with new
 """
@@ -11,6 +13,10 @@ def rreplace(s: str, old: str, new: str, occurrence: int) -> str:
     li = s.rsplit(old, occurrence)
     return new.join(li)
 
+"""
+Runs ../make-visualization.sh script to turn h5 to png.
+Also renames output png to the name of the directory it's in
+"""
 def visualize_gauss(mainDataFile: str):
     subprocess.run(["../make-visualization.sh", mainDataFile])
     # chdir so we don't need to use absolute path when renaming output image
@@ -19,6 +25,10 @@ def visualize_gauss(mainDataFile: str):
     os.rename(rreplace(os.path.abspath(mainDataFile), ".h5", ".png", 1),
               containing_dir_name + ".png")
 
+"""
+Get iterator containg directory names that match pattern "amp=* logk=*"
+Only directories in current directory, names relative to current directory
+"""
 def get_dirs_to_visualize() -> filter:
     regex_pattern = "^amp=[0-9\.]+ logk=[+-]?[0-9\.]+$"
     regex = re.compile(regex_pattern)
@@ -28,6 +38,6 @@ def get_dirs_to_visualize() -> filter:
 
 if __name__ == "__main__":
     print("Note. Run this file in directory containing Gauss2d.py and output directories")
-    dirs_to_visualize = map(lambda dir: dir + "/Gauss2d-ez-000003696.h5", get_dirs_to_visualize())
+    dirs_to_visualize = map(lambda dir: dir + OUTPUT_H5_FILENAME, get_dirs_to_visualize())
     with multiprocessing.Pool() as pool:
         pool.map(visualize_gauss, dirs_to_visualize)
